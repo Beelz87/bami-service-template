e21672714e4b53a2041cfc792c81bb3b1d4dd255
beel <hpit.vie@gmail.com>
2025-12-26 17:30:33 +0700

    add authorization mechanism: rbac policy

---
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/authz/provider.py
@@ -0,0 +1,8 @@
+from bami_chassis.security.authz.casbin_provider import CasbinProvider
+from importlib import resources
+
+
+def service_enforcer_provider():
+    pkg = "{{service_package}}.resources.authz"
+    with resources.as_file(resources.files(pkg) / "policy.csv") as p:
+        return CasbinProvider(policy_path=str(p))
