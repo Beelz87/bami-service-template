01cba7bc413b0a3b48cefd53625efcfaa29623f8
beelz <hpit.vie@gmail.com>
2025-12-27 10:05:40 +0700

    fix bugs: - error: fill service name when generate a new service by cookiecutter

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/authz/provider.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/authz/provider.py
@@ -3,6 +3,6 @@ from importlib import resources
 
 
 def service_enforcer_provider():
-    pkg = "{{service_package}}.resources.authz"
+    pkg = "{{cookiecutter.package_name}}.resources.authz"
     with resources.as_file(resources.files(pkg) / "policy.csv") as p:
         return CasbinProvider(policy_path=str(p))
