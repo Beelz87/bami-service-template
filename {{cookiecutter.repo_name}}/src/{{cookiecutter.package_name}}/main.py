ee11b5dfe2f23805a98fdb622a1b987c34ba06da
beelz <hpit.vie@gmail.com>
2026-01-04 23:59:31 +0700

    update

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/main.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/main.py
@@ -1,31 +1,5 @@
-from bami_chassis.interfaces.api import metrics_router
-from bami_chassis.interfaces.api.health import health_router
-from fastapi import FastAPI
-from bami_chassis import (
-    settings,
-    RequestLoggingMiddleware,
-    MetricsMiddleware,
-    TracingMiddleware,
-    init_tracer,
-    instrument_app,
+from bami_chassis.bootstrap.app_factory.api_app_factory import create_app
+
+app = create_app(
+    service_name="bami-service"
 )
-
-def create_app() -> FastAPI:
-    app = FastAPI(
-        title="{{cookiecutter.service_name}} service",
-        debug=settings.debug,
-    )
-
-    app.add_middleware(MetricsMiddleware)
-
-    if settings.enable_tracing:
-        init_tracer()
-        instrument_app(app)
-        app.add_middleware(TracingMiddleware)
-
-    app.include_router(health_router)
-    app.include_router(metrics_router)
-
-    return app
-
-app = create_app()
